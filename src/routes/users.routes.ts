c2d723ea34bd71c667397c4f8a5ebb3df927cab0
douglasdsda <douglasdsda@gmail.com>
2020-06-28 11:52:45 -0300

    upload static imagem

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,7 +1,12 @@
 import { Router } from 'express';
+import multer from 'multer';
 import CreateUsersService from '../services/CreateUsersService';
+import UpdateUserAvatarService from '../services/UpdateUserAvatarService';
+import ensureAuthenticated from '../middlewares/ensureAuthenticated';
+import uploadConfig from '../config/upload';
 
 const userRouter = Router();
+const upload = multer(uploadConfig);
 
 userRouter.post('/', async (request, response) => {
     try {
@@ -22,5 +27,27 @@ userRouter.post('/', async (request, response) => {
         return response.status(400).json({ error: err.message });
     }
 });
+
+userRouter.patch(
+    '/avatar',
+    ensureAuthenticated,
+    upload.single('avatar'),
+    async (request, response) => {
+        try {
+            const updateUserAvatarService = new UpdateUserAvatarService();
+
+            const user = await updateUserAvatarService.execute({
+                user_id: request.user.id,
+                avatarFilename: request.file.filename,
+            });
+
+            delete user.password;
+
+            return response.json(user);
+        } catch (err) {
+            return response.status(400).json({ error: err.message });
+        }
+    },
+);
 
 export default userRouter;
